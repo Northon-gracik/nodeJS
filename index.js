3f6e196f23080cffdac8afb5b2a5ce7792a39832
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-02-04 21:34:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require("express");
+import express from 'express';
+import cors from 'cors';
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => res.json({ ok: true}))
